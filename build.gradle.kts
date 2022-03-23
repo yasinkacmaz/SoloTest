d650c7c39b4c121a89010a6dcd9b7a675d920f12
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-03-24 00:56:20 +0300

    Use kotlin dsl at gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.1.2" apply false
+    id("com.android.library") version "7.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+import org.gradle.api.JavaVersion
+
+object Config {
+    const val compileSdk = 32
+    const val minSdk = 24
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+    val JAVA_VERSION = JavaVersion.VERSION_11
+}
