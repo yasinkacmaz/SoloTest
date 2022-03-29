a4a168a63edaee34fdae525760eba5a2161c6fd4
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-03-29 23:31:44 +0300

    Render pegs and board separately

--- app/src/main/java/com/yasinkacmaz/solotest/SoloTestGame.kt
+++ app/src/main/java/com/yasinkacmaz/solotest/SoloTestGame.kt
@@ -1,25 +1,21 @@
 package com.yasinkacmaz.solotest
 
-import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.unit.dp
-import com.yasinkacmaz.solotest.PegOffsetValidator.isValidOffset
 import com.yasinkacmaz.solotest.PegOffsetCalculator.offsetOfBoardIndex
+import com.yasinkacmaz.solotest.PegOffsetValidator.isValidOffset
 
 @Composable
 fun SoloTestGame(
     modifier: Modifier = Modifier,
     boardConfig: BoardConfig,
     playableBoardIndexes: MutableList<Int>,
-    pegs: List<Peg>,
-    selectedPegIndex: MutableState<Int?>
+    pegs: List<Peg>
 ) = Canvas(modifier) {
     drawCircle(
         color = Color.Red,
@@ -29,28 +25,24 @@ fun SoloTestGame(
     )
 
     playableBoardIndexes.forEach { boardIndex ->
-        val peg = pegs.find { peg -> peg.boardIndex == boardIndex }
-        val offset = peg?.offset ?: boardConfig offsetOfBoardIndex boardIndex
-        val style = if (peg != null) Fill else Stroke(width = 1.5.dp.toPx())
-        print(selectedPegIndex)
+        val offset = boardConfig offsetOfBoardIndex boardIndex
+        drawCircle(
+            color = Color.Green,
+            center = offset,
+            radius = boardConfig.pegSize / 2,
+            style = Stroke(width = 1.5.dp.toPx())
+        )
+    }
+
+    pegs.forEach { peg ->
+        val offset = peg.offset
+        // TODO: No need to control after snapping added
         val color = if (boardConfig.isValidOffset(offset)) Color.Green else Color.Red
         drawCircle(
             color = color,
             center = offset,
             radius = boardConfig.pegSize / 2,
-            style = style
+            style = Fill
         )
-        drawContext.canvas.nativeCanvas.apply {
-            drawText(
-                boardIndex.toString(),
-                offset.x,
-                offset.y + 10,
-                Paint().apply {
-                    textSize = 50F
-                    this.color = android.graphics.Color.WHITE
-                    textAlign = Paint.Align.CENTER
-                }
-            )
-        }
     }
 }
