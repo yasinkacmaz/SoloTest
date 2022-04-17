76f1ee734558e16394e86f967a8b246eaa644605
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-04-17 14:13:00 +0300

    Create signing config and release build variant

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Config {
-    const val compileSdk = 32
     const val minSdk = 24
+    const val compileSdk = 32
     const val targetSdk = 32
     const val versionCode = 1
     const val versionName = "1.0.0"
