09c3ccfae64652abada73cddf8f7723ef36928f0
Yasin Kaçmaz <yasinkacmaz57@gmail.com>
2022-04-29 15:49:33 +0300

    Fix game over bug when three pegs left side by side (#30)

--- app/src/main/java/com/yasinkacmaz/solotest/logic/GameOverDetector.kt
+++ app/src/main/java/com/yasinkacmaz/solotest/logic/GameOverDetector.kt
@@ -1,5 +1,6 @@
 package com.yasinkacmaz.solotest.logic
 
+import com.yasinkacmaz.solotest.data.BoardConfig
 import com.yasinkacmaz.solotest.util.Direction
 import com.yasinkacmaz.solotest.data.Peg
 import com.yasinkacmaz.solotest.data.Remaining
@@ -7,11 +8,11 @@ import com.yasinkacmaz.solotest.util.toBoardIndex
 import com.yasinkacmaz.solotest.util.toRowAndColumn
 
 object GameOverDetector {
-    fun isGameOver(pegs: List<Peg>, gridSize: Int): Boolean {
+    fun isGameOver(pegs: List<Peg>, boardConfig: BoardConfig): Boolean {
         if (Remaining.of(pegs.size) == null) return false
 
         return pegs.none { peg ->
-            val (row, column) = peg.boardIndex.toRowAndColumn(gridSize)
+            val (row, column) = peg.boardIndex.toRowAndColumn(boardConfig.gridSize)
             Direction.values.any { direction ->
                 val movedPlace = when (direction) {
                     Direction.UP -> row - 2 to column
@@ -19,7 +20,8 @@ object GameOverDetector {
                     Direction.LEFT -> row to column - 2
                     Direction.RIGHT -> row to column + 2
                 }
-                PegMovementValidator.isMovementValid(pegs, peg, movedPlace.toBoardIndex(gridSize), gridSize)
+                val boardIndex = movedPlace.toBoardIndex(boardConfig.gridSize)
+                PegMovementValidator.isMovementValid(pegs, peg, boardIndex, boardConfig)
             }
         }
     }
